0b17467d5c5b77037164dd64222ba1e952bc0ec4
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-09-04 04:27:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const Api="https://storage.googleapis.com/acciojob-open-file-collections/lorem-redux.gif"
   return (
     <div>
         {/* Do not remove the main div */}
