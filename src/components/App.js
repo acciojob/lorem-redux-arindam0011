b06aa51588e8ccc21af3727b2ae7f4ebccddcbb6
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-09-13 02:09:36 +0530

    1.1

--- src/components/App.js
+++ src/components/App.js
@@ -7,26 +7,36 @@ import 'regenerator-runtime/runtime';
 const App = () => {
   const dispatch=useDispatch();
   const data=useSelector((state)=>state.loremDAta.data);
-  const Api="https://storage.googleapis.com/acciojob-open-file-collections/lorem-redux.gif"
+  const Api="https://jsonplaceholder.typicode.com/posts"
 
   useEffect(() => {
     const getData = async () => {
-      // Fetch the image as a Blob and create a URL
       const res = await fetch(Api);
-      const blob = await res.blob();
-      const imageUrl = URL.createObjectURL(blob);
-      
-      // Dispatch the URL to the Redux store
-      dispatch(addData(imageUrl));
+      const data = await res.json();
+      console.log(data.slice(0, 9))
+      dispatch(addData(data.slice(0, 9)));
     };
     getData();
-  }, [dispatch]);
+  }, []);
 
   return (
     <div>
-        {
-          <img src={data} alt=""/>
-        }
+        <h1>A short nation of Lorem Ipsum</h1>
+        <h4>Below contentent is getten from a random API. Please take you timr to review it.</h4>
+        <div style={{border:"1px solid black", padding:"10px", display:"flex", flexWrap:"wrap"}}>
+          {
+            data.map((item, index) => {
+              return (
+                <div key={index} style={{border:"1px solid black", padding:"10px", margin:"10px" , width:"300px"}}>
+                  <ul style={{listStyle:"none"}}>
+                  <li><span style={{fontWeight:"bold"}}>Title: </span>{item.title}</li>
+                  <li><span style={{fontWeight:"bold"}}>Body: </span>{item.body}</li>
+                  </ul>
+                </div>
+              )
+            })
+          }
+        </div>
     </div>
   )
 }
