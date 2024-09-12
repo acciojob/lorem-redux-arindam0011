7b02474e14a9869b76132f4a227f0a57dbaa344e
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-09-13 02:16:29 +0530

    1.2

--- src/components/App.js
+++ src/components/App.js
@@ -14,29 +14,32 @@ const App = () => {
       const res = await fetch(Api);
       const data = await res.json();
       console.log(data.slice(0, 9))
-      dispatch(addData(data.slice(0, 9)));
+      dispatch(addData(data.slice(0, 9) || []));
     };
     getData();
   }, []);
 
   return (
     <div>
-        <h1>A short nation of Lorem Ipsum</h1>
-        <h4>Below contentent is getten from a random API. Please take you timr to review it.</h4>
-        <div style={{border:"1px solid black", padding:"10px", display:"flex", flexWrap:"wrap"}}>
+        <h1>A short Naration of Lorem Ipsum</h1>
+        <h4>Below Contains A title and Body gotten froma random API, Please take your time to Review</h4>
+        {
+
+        data? (<div style={{border:"1px solid black", padding:"10px", display:"flex", flexWrap:"wrap"}}>
           {
             data.map((item, index) => {
               return (
                 <div key={index} style={{border:"1px solid black", padding:"10px", margin:"10px" , width:"300px"}}>
                   <ul style={{listStyle:"none"}}>
-                  <li><span style={{fontWeight:"bold"}}>Title: </span>{item.title}</li>
-                  <li><span style={{fontWeight:"bold"}}>Body: </span>{item.body}</li>
+                  <li className="title"><span style={{fontWeight:"bold"}}>Title: </span>{item.title}</li>
+                  <li className="body"><span style={{fontWeight:"bold"}}>Body: </span>{item.body}</li>
                   </ul>
                 </div>
               )
             })
           }
-        </div>
+        </div>): <div>Loading...</div>
+        }
     </div>
   )
 }
