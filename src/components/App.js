e19b401b68c4e86003959a876936905ee0acf3b3
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-09-13 02:21:56 +0530

    1.3

--- src/components/App.js
+++ src/components/App.js
@@ -5,41 +5,34 @@ import { useSelector, useDispatch } from "react-redux";
 import { addData } from "../components/LoremData";
 import 'regenerator-runtime/runtime';
 const App = () => {
-  const dispatch=useDispatch();
-  const data=useSelector((state)=>state.loremDAta.data);
-  const Api="https://jsonplaceholder.typicode.com/posts"
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.loremDAta.data);
+  const Api = "https://jsonplaceholder.typicode.com/posts"
 
   useEffect(() => {
     const getData = async () => {
       const res = await fetch(Api);
       const data = await res.json();
-      console.log(data.slice(0, 9))
-      dispatch(addData(data.slice(0, 9) || []));
+      console.log(data[1])
+      dispatch(addData(data[1] || []));
     };
     getData();
   }, []);
 
   return (
     <div>
-        <h1>A short Naration of Lorem Ipsum</h1>
-        <h4>Below Contains A title and Body gotten froma random API, Please take your time to Review</h4>
-        {
+      <h1>A short Naration of Lorem Ipsum</h1>
+      <h4>Below Contains A title and Body gotten froma random API, Please take your time to Review</h4>
+      {
 
-        data? (<div style={{border:"1px solid black", padding:"10px", display:"flex", flexWrap:"wrap"}}>
-          {
-            data.map((item, index) => {
-              return (
-                <div key={index} style={{border:"1px solid black", padding:"10px", margin:"10px" , width:"300px"}}>
-                  <ul style={{listStyle:"none"}}>
-                  <li className="title"><span style={{fontWeight:"bold"}}>Title: </span>{item.title}</li>
-                  <li className="body"><span style={{fontWeight:"bold"}}>Body: </span>{item.body}</li>
-                  </ul>
-                </div>
-              )
-            })
-          }
-        </div>): <div>Loading...</div>
-        }
+        data ? (
+            <ul style={{ listStyle: "none" }}>
+              <li className="title"><span style={{ fontWeight: "bold" }}>Title: </span>{data.title}</li>
+              <li className="body"><span style={{ fontWeight: "bold" }}>Body: </span>{data.body}</li>
+            </ul>
+          )
+          : <div>Loading...</div>
+      }
     </div>
   )
 }
