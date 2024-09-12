70d25587a653817e7d29e2319ddca3af05d696eb
ARINDAM BHANDARI <arindambhandari5@gmail.com>
2024-09-13 03:46:33 +0530

    jfd

--- src/components/App.js
+++ src/components/App.js
@@ -15,10 +15,7 @@ const App = () => {
       try {
         const res = await fetch(Api);
         const fetchedData = await res.json();
-        setTimeout(() => {
-          
-          dispatch(addData(fetchedData.slice(0, 10) || []));
-        }, 4000);
+        dispatch(addData(fetchedData.slice(0, 10) || []));
       } catch (error) {
         dispatch(setError("Failed to fetch data"));
       }
@@ -30,15 +27,17 @@ const App = () => {
     <div>
       <h1>A short Naration of Lorem Ipsum</h1>
       <h4>Below Contains A title and Body gotten froma random API, Please take your time to Review</h4>
-      {data.length > 0 ? (<div>
-          {data.map((post, index) => (
+      <div>
+       { (<div>
+          { data.map((post, index) => (
             <ul key={index} style={{ listStyle: "none", textAlign: "left", padding: "0" }}>
               <li className="title"><span style={{ fontWeight: "bold" }}>Title: </span>{post.title}</li>
               <li className="body"><span style={{ fontWeight: "bold" }}>Body: </span>{post.body}</li>
             </ul>
           ))}
-        </div>
-      ) : <div>loading state</div>} 
+        </div>) || <div>loading state</div>
+        }
+      </div>
     </div>
   );
 }
